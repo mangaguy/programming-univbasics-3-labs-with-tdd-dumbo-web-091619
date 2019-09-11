e9683c4d990833e63b7dc8a181777ff256e7a293
artificial-semicolon-2761 <jimmy.h.cheung@gmail.com>
2019-09-11 06:38:54 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,9 +3,9 @@ first_number = 10
 
 second_number = 3
 
-sum = first_number+second_number
+sum = first_number + second_number
 
-difference = first_number-second_number
+difference = first_number - second_number
 
 product = first_number * second_number
 
